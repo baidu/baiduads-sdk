9e4b724f9827a2e87c151a64c84501a1b146f4c4
jiangyuan04 <jiangyuan04@baidu.com>
2022-04-07 19:35:28 +0800

    2022.2.1.5-SNAPSHOT

--- java/baiduads-sdk-auto/src/main/java/com/baidu/dev2/api/sdk/invoke/ApiClient.java
+++ java/baiduads-sdk-auto/src/main/java/com/baidu/dev2/api/sdk/invoke/ApiClient.java
@@ -123,7 +123,7 @@ public class ApiClient extends JavaTimeFormatter {
     dateFormat = ApiClient.buildDefaultDateFormat();
 
     // Set default User-Agent.
-    setUserAgent("baiduads-sdk/2022.2.1.4-SNAPSHOT/java");
+    setUserAgent("baiduads-sdk/2022.2.1.5-SNAPSHOT/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
